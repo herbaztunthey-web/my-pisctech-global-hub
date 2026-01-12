51a17337505839bd7d4be40e5f747aa85374a19c
herbaztunthey-web <herbaztunthey@gmail.com>
2026-01-13 01:54:40 +0400

    Fix ModuleNotFoundError and add weather routes

--- app.py
+++ app.py
@@ -1,36 +1,37 @@
-import os
-import requests
 from flask import Flask, render_template, request, jsonify
+import requests  # The library we just added to requirements.txt
 
 app = Flask(__name__)
 
-# Replace with your actual key from your weather app info
-API_KEY = 'aad4cbdae56d6d693c4f99064fe46dcd'
+# Use the API key you mentioned having for your weather app
+API_KEY = 'YOUR_API_KEY_HERE'
 
 
 @app.route('/')
-def home():
+def index():
+    # Make sure your file is named index.html in the templates folder
     return render_template('index.html')
 
 
 @app.route('/weather')
-def weather_page():
+def weather():
     return render_template('weather.html')
 
 
 @app.route('/maritime')
-def maritime_page():
+def maritime():
     return render_template('maritime.html')
 
 
 @app.route('/solar')
-def solar_page():
+def solar():
     return render_template('solar.html')
 
 
 @app.route('/get_weather')
-def get_weather():
+def get_weather_data():
     city = request.args.get('city')
+    # This talks to the OpenWeather API using the 'requests' module
     url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
     response = requests.get(url)
     return jsonify(response.json())
